06a7a5c0aef637b0ae34a8dd389199f1cbca62c3
greg <greg.sandstrom@gmail.com>
2013-05-15 00:16:52 -0400

    Add NetMF 4.1 version

---
+++ Xively/StringBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Xively
+{
+#if MF_FRAMEWORK_VERSION_V4_1
+    [Obsolete("NETMF 4.1 Provides a proper implementation of this.")]
+    internal class StringBuilder
+    {
+        private string buffer;
+
+        public StringBuilder(string p)
+        {
+            this.buffer = p;
+        }
+
+        public StringBuilder Append(string value)
+        {
+            buffer += value;
+            return this;
+        }
+
+        public StringBuilder Append(double value)
+        {
+            buffer += value.ToString();
+            return this;
+        }
+    }
+#endif
+}
